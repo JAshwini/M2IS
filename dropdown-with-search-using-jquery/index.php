f7447dce253aedcd7b59fb8b43240a209da01649
Gaurav Jiandani <jiandanigaurav@gmail.com>
2017-11-08 16:18:35 +0530

    Front end files

---
+++ dropdown-with-search-using-jquery/index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				var country = ["Australia", "Bangladesh", "Denmark", "Hong Kong", "Indonesia", "Netherlands", "New Zealand", "South Africa"];
+				$("#country").select2({
+				  data: country
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<h1>DropDown with Search using jQuery</h1>
+		<div>
+			<select id="country" style="width:300px;">
+			<!-- Dropdown List Option -->
+			</select>
+		</div>
+	</body>
+</html>
